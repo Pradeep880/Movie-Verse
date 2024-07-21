b94130c2e16be35d339f891d855e26c8674f324a
pradeep kushwaha <pkmaurya112000@mgmail.com>
2024-07-22 00:02:33 +0530

    logout added

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const router=express.Router();
 
-const {login,signup}=require("../Auth/authentication");
+const {login,signup,logout}=require("../Auth/authentication");
 // const {auth,isStudent,isAdmin}=require('../middleware/auth')
 router.post('/login',login);
 router.post('/signup',signup);
-
+// router.post('/logout',logout);
 
 //for testing purpose
 router.get('/',(req,res)=>{
